2fc46bf912043d8fbfdc7517e8a120e7f35678bc
Daniel <danielspeixoto@outlook.com>
2017-12-08 21:37:53 +0000

    small setup

---
+++ os-simulator/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, compose, applyMiddleware} from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+const middlewares = [
+	thunk,
+	createLogger()
+  ];
+
+export default createStore(
+    reducers,
+    undefined,
+	compose(
+	  applyMiddleware(...middlewares),
+	  window.devToolsExtension ? window.devToolsExtension() : f => f
+	)
+);
