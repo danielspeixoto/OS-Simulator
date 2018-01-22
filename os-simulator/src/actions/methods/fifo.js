37d4c60e1e79922df348c19caf67a9bb2855faf0
Fernando Passos <30850453+fernandodmp@users.noreply.github.com>
2018-01-22 16:17:12 -0300

    Updated FIFO with LRU

--- os-simulator/src/actions/methods/fifo.js
+++ os-simulator/src/actions/methods/fifo.js
@@ -12,6 +12,7 @@ const fifo = (processes, memoryMethod, quantum) => {
 	var i = 0
 	var time = 0
 	var turnaround = 0
+	var k = 0
 	if(memoryMethod === 'fifo') {
 		processes.forEach(process => {
 			// If there is no processes starting at current time, wait
@@ -26,14 +27,16 @@ const fifo = (processes, memoryMethod, quantum) => {
 			}
 			
 			// Loading pages lasts a quantum for each page
+			for(i = 0; i < quantum * process.numOfPages; i++) {
+				timeline.push(null)
+				memoryTimeline.push(
+					memory.slice()
+				)
+				time++
+			}
+
+			// Loads page
 			for(i = 0; i < process.numOfPages; i++) {
-				for(let j = 0; j < quantum; j++) {
-					timeline.push(null)
-					memoryTimeline.push(
-						memory.slice()
-					)
-					time++
-				}
 				memory[pointerToPage] = process.number
 				pointerToPage = (pointerToPage + 1) % 100
 			}
@@ -50,18 +53,80 @@ const fifo = (processes, memoryMethod, quantum) => {
 				)
 				time++
 			}
-			turnaround += time - process.startTime
+			turnaround+=time - process.startTime
 		})
 	}
-
-	// TODO Assure floating number
+	else{
+		var ages = memory.map(p => p)
+		processes.forEach(process => {
+			// If there is no processes starting at current time, wait
+			if(time < process.startTime) {
+				for(; time < process.startTime; time++) {
+					// Null means that there are no processes runnning
+					timeline.push(null)
+					memoryTimeline.push(
+						memory.slice()
+					)
+				}
+			}
+			// Loading pages lasts a quantum for each page
+			for(i = 0; i < quantum * (process.numOfPages - process.loadedPages); i++) {
+				timeline.push(null)
+				memoryTimeline.push(
+					memory.slice()
+				)
+				time++
+			}
+			for(i = 0; i < (process.numOfPages - process.loadedPages);) {				
+				var menor = time
+				for(k = 0; k < 100; k++){
+					if(ages[k] === null){
+						pointerToPage = k
+						break
+					}
+					if(ages[k] < menor){
+						menor = ages[k]
+						pointerToPage = k
+					}
+				}
+				for(k = 0; k < processes.length; k++){
+					if(processes[k].number == memory[pointerToPage]){
+						processes[k].loadedPages--
+					}
+				}
+				memory[pointerToPage] = process.number
+				ages[pointerToPage] = time
+				process.loadedPages++
+			}
+			for(k = 0; k < 100; k++){
+				if(memory[k] == process.number){
+					ages[k] = time;
+				}
+			}
+			// Execute process
+			duration = process.duration;
+			for(i = 0; i < duration; i++) {
+				timeline.push({
+					number: process.number,
+					isOverride: false
+				})
+				memoryTimeline.push(
+					memory.slice()
+				)
+				time++
+			}
+			turnaround+=time - process.startTime
+		})
+	}
+	turnaround = turnaround/processes.length
 	return {
 		timeline,
 		memoryTimeline,
-		turnaround: turnaround / processes.length
+		turnaround
 	}
 }
 
+//TODO Return ints instead of strings
 function compare(a, b) {
 	if(a.startTime < b.startTime) {
 		return -1
